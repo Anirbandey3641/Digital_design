9404052cc7e6cd6bee1104c3239e7a67b866ce11
Anirban Dey <dey364192@gmail.com>
2026-09-16 23:26:25 +0530

    Create 2x4 decoder module in Verilog
    Added a 2x4 decoder module with input and output assignments.

--- 2x4 Decoder/rtl/2x4_Decoder.v
+++ 2x4 Decoder/rtl/2x4_Decoder.v
@@ -1,1 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.06.2026 22:06:32
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module decoder(
+  input a,
+  input b,
+  output o1,
+  output o2,
+  output o3,
+  output o4
+);
+  assign o1= (~a) & (~b);
+  assign o2= (~a) & b;
+  assign o3= a & (~b);
+  assign o4= a & b;
+endmodule
